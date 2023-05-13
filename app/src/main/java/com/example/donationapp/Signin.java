5ace27df31e1c5202c39d194ca72aab8ebf662ce
Wendy <lebol2337@gmail.com>
2023-05-13 17:58:52 +0200

    #loginchanges
    login changes

--- app/src/main/java/com/example/donationapp/Signin.java
+++ app/src/main/java/com/example/donationapp/Signin.java
@@ -9,6 +9,7 @@ public class Signin extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_signin);
     }
 }
